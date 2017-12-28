7c6c895b820324e5838518be01064286d70a58df
Bert Bullough <bert@bertbullough.com>
2017-12-28 10:20:26 -0700

    List - Section 15

--- Section 4 - Lists/listNotes.py
+++ Section 4 - Lists/listNotes.py
@@ -49,3 +49,87 @@ True
 False
 42 not in ['hello', 'hi', 'howdy', 'heyas']
 True
+
+>>> for i in range(4):
+...     print(i)
+...
+0
+1
+2
+3
+
+>>> for i in [0, 1, 2, 3]:
+...     print(i)
+...
+0
+1
+2
+3
+
+>>> list(range(4))
+[0, 1, 2, 3]
+>> list(range(0, 100, 2))
+[0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 88, 90, 92, 94, 96, 98]
+
+>>> supplies = ['pens', 'staplers', 'flame-throwers', 'binders']
+>>> for i in range(len(supplies)):
+...     print('Index ' + str(i) + ' in supplies is: ' + supplies[i])
+...
+Index 0 in supplies is: pens
+Index 1 in supplies is: staplers
+Index 2 in supplies is: flame-throwers
+Index 3 in supplies is: binders
+
+
+>>> supplies = ['pens','pens','pens','pens','pens','pens','pens','pens','pens','pens','pens','pens']
+>>> for i in range(len(supplies)):
+...     print('Index ' + str(i) + ' in supplies is: ' + supplies[i])
+...
+Index 0 in supplies is: pens
+Index 1 in supplies is: pens
+Index 2 in supplies is: pens
+Index 3 in supplies is: pens
+Index 4 in supplies is: pens
+Index 5 in supplies is: pens
+Index 6 in supplies is: pens
+Index 7 in supplies is: pens
+Index 8 in supplies is: pens
+Index 9 in supplies is: pens
+Index 10 in supplies is: pens
+Index 11 in supplies is: pens
+
+##multiple assignment
+
+>>> cat = ['fat', 'orange', 'loud']
+>>> size = cat[0]
+>>> color = cat[1]
+>>> disposition = cat[2]
+>>>
+>>>
+>>> size, color, disposition = cat
+>>> size
+'fat'
+>>> color
+'orange'
+>>> disposition
+'loud'
+
+>>> a = 'AAA'
+>>> b = 'BBB'
+>>>
+>>> a,b = b,a
+>>> a
+'BBB'
+>>> b
+'AAA'
+
+##Augmented assignment operators
+
+>>> spam = 42
+>>> spam = spam + 1
+>>> spam
+43
+
+>>> spam += 1
+>>> spam
+44
