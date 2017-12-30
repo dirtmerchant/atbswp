75438124e4dae0235343768d9f68d2ad1ab512df
Bert Bullough <bert@bertbullough.com>
2017-12-29 20:36:59 -0700

    data structure

--- Section 5 - Dictionaries/dataStructureNotes.py
+++ Section 5 - Dictionaries/dataStructureNotes.py
@@ -27,4 +27,19 @@
  'top-M': ' ',
  'top-R': ' '}
 
+ >> def printBoard(board):
+...        print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+...        print('-+-+-')
+...        print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+...        print('-+-+-')
+...        print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+...
+>>>
+>>> printBoard(theBoard)
+ | |
+-+-+-
+ | |
+-+-+-
+ | |
+
  
