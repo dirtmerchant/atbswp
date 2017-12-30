47ae61248db06ea9581d29b34eabffe5c0d1efbd
Bert Bullough <bert@bertbullough.com>
2017-12-29 20:29:02 -0700

    data structure notes initial commit

---
+++ Section 5 - Dictionaries/dataStructureNotes.py
@@ -0,0 +1,8 @@
+>>> allCats = []
+>>> allCats.append({'name': 'Zophie', 'age': 7, 'color': 'gray'})
+>>> allCats.append({'name': 'Pooka', 'age': 5, 'color': 'black'})
+>>> allCats.append({'name': 'Fat-tail', 'age': 5, 'color': 'orange'})
+>>> allCats.append({'name': '???', 'age': -1, 'color': 'orange'})
+>>>
+>>> allCats
+[{'name': 'Zophie', 'age': 7, 'color': 'gray'}, {'name': 'Pooka', 'age': 5, 'color': 'black'}, {'name': 'Fat-tail', 'age': 5, 'color': 'orange'}, {'name': '???', 'age': -1, 'color': 'orange'}]
