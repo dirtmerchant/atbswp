277df4e0c95b1c1b52b2f3185709f7c67acf4504
Bert Bullough <bert@bertbullough.com>
2017-12-29 20:05:16 -0700

    pformat

--- Section 5 - Dictionaries/characterCount.py
+++ Section 5 - Dictionaries/characterCount.py
@@ -4861,4 +4861,5 @@ for character in message.upper():
     count.setdefault(character, 0)
     count[character] = count[character] + 1
 
-pprint.pprint(count)
+rjtext = pprint.pformat(count)
+print(rjtext)
