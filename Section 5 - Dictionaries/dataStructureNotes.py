c4c6cd7ad43b31b42ddddb0b718251f99524999a
Bert Bullough <bert@bertbullough.com>
2017-12-29 20:32:24 -0700

    tic tac toe

--- Section 5 - Dictionaries/dataStructureNotes.py
+++ Section 5 - Dictionaries/dataStructureNotes.py
@@ -6,3 +6,25 @@
 >>>
 >>> allCats
 [{'name': 'Zophie', 'age': 7, 'color': 'gray'}, {'name': 'Pooka', 'age': 5, 'color': 'black'}, {'name': 'Fat-tail', 'age': 5, 'color': 'orange'}, {'name': '???', 'age': -1, 'color': 'orange'}]
+
+>>> theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+...             'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+...             'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+>>>
+
+>>> print(theBoard)
+{'top-L': ' ', 'top-M': ' ', 'top-R': ' ', 'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ', 'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+
+>>> import pprint
+>>> pprint.pprint(theBoard)
+{'low-L': ' ',
+ 'low-M': ' ',
+ 'low-R': ' ',
+ 'mid-L': ' ',
+ 'mid-M': ' ',
+ 'mid-R': ' ',
+ 'top-L': ' ',
+ 'top-M': ' ',
+ 'top-R': ' '}
+
+ 
