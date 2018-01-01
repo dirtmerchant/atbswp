dfea8ad95ac3d1b0353d7af87fc7009cb0f3a5cb
Bert Bullough <bert@bertbullough.com>
2018-01-01 10:09:37 -0700

    string manipulation notes

---
+++ Section 6 - Manipulating Strings/stringNotes.py
@@ -0,0 +1,122 @@
+... """
+>>> print(len(spam))
+132213
+>>> spam = 'Hello world!'
+>>>
+>>>
+>>>
+>>> spam[1:5]
+'ello'
+>>> spam[-1]
+'!'
+>>> 'x' in spam
+False
+>>> 'Hello' in spam
+True
+>>>
+>>>
+>>>
+>>> spam = 'Hello world'
+>>> spam.upper()
+'HELLO WORLD'
+>>> answer = input()
+yes
+>>> if answer == 'yes':
+...     print('Playing again')
+...
+Playing again
+>>> answer.lower() == 'yes'
+True
+>>>
+>>>
+>>>
+>>> spam = "Hello world!/home/attask/bin/maintenance -d/home/attask/bin/maintenance -d'
+KeyboardInterrupt
+>>> spam = 'Hello world!'
+>>> spam.islower()
+False
+>>>
+>>> spam.isupper()
+False
+>>> spam = ''
+>>> spam.isupper()
+False
+>>> spam.islower()
+False
+>>>
+>>>
+>>>
+>>>
+>>> spam
+''
+>>>
+>>>
+>>>
+>>> 'Hello world!'.isspace()
+False
+>>> 'Hello world!'[5].isspace()
+True
+>>> 'Hello world!'.startswith('Hello')
+True
+>>> 'Hello world!'.startswith('H')
+True
+>>> 'Hello world!'.startswith('ello')
+False
+>>>
+>>>
+>>> 'Hello world!'.endswith('world'
+... )
+False
+>>> 'Hello world!'.endswith('world!')
+True
+>>>
+
+
+>>> 'Hello'.rjust(10)
+'     Hello'
+>>>  'Hello'.rjust(10)
+KeyboardInterrupt
+>>> 'Hello'.ljust(10)
+'Hello     '
+>>>
+
+>>>
+>>> 'Hello'.rjust(20, '*')
+'***************Hello'
+>>> 'Hello'.ljust(20, '-')
+'Hello---------------'
+>>> 'Hello'.center(20)
+'       Hello        '
+>>> 'Hello'.center(20, '=')
+'=======Hello========'
+>>> name = 'Al'
+>>> name.center(20, '=')
+'=========Al========='
+>>>
+
+
+>>> spam = spam.strip()
+>>> spam
+'Hello'
+>>> spam = 'SpamSpamBaconSpamEggsSpamSpam'
+>>>
+>>> spam.strip('ampS')
+'BaconSpamEggs'
+>>>
+>>>
+>>> spam = 'Hello there!'
+>>> spam.repplace('e', 'XYZ')
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+AttributeError: 'str' object has no attribute 'repplace'
+>>> spam.replace('e', 'XYZ')
+'HXYZllo thXYZrXYZ!'
+
+
+
+>> import pyperclip
+>>>
+>>> pyperclip.copy('Hello!!!!')
+>>> pyperclip.paste()
+'Hello!!!!'
+>>>
